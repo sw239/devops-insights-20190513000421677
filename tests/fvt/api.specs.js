2da7efbfaff21cb6726d5f74039b39fda8395285
shizhen wang <353311544@qq.com>
2019-05-14 06:43:24 +0000

    e

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -1,77 +1,90 @@
-
 (function () {
 
-    'use strict';
+	'use strict';
 
-    var apiv1 = require('../../routes/apiv1');
-    var assert = require('chai').assert;
-    var REQUEST = require('request');
+	var webdriver = require('selenium-webdriver');
+	var By = require('selenium-webdriver').By;
+	var until = require('selenium-webdriver').until;
+	var assert = require('assert');
+	var sauceLabs = require('saucelabs');
+    
+	var SAUCELABSURL = 'http://ondemand.saucelabs.com:80/wd/hub';
+	var DEMODRAURL = 'https://demodratest.mybluemix.net/#/';
 
-    var request = REQUEST.defaults( {
-        strictSSL: false
+	var driver = new webdriver.Builder().
+        			usingServer(SAUCELABSURL).
+        			withCapabilities(
+        				{
+        					browserName: 'Firefox',
+        					platform: 'Windows XP',
+        					name: 'Current Weather Report check',
+        					tags: ["demodra"],
+        					build: '1.1.0',
+        					username: process.env.SAUCE_USERNAME,
+        					accessKey: process.env.SAUCE_ACCESS_KEY
+    					}
+    				).build();
+
+  var saucelab = new sauceLabs({
+          username: process.env.SAUCE_USERNAME,
+          password: process.env.SAUCE_ACCESS_KEY
+        });
+
+	describe('Test current weather report', function() {
+
+    before(function(done) {
+      driver.get(DEMODRAURL);
+      done();
     });
 
-    var appUrl = process.env.APP_URL;
-	console.log('URL'+ appUrl);
-	
-    describe('Get Weather', function() {
-
-    	it('with valid zip code', function(done) {
-        if(!appUrl) {
-            assert.fail("Environment variable APP_URL is not defined");
-            return done();
-        }
-        request({
-      		method: 'GET',
-              url: appUrl + '/api/v1/getWeather?zip=Hamilton'
-          }, function(err, resp, body) {
-          	if(err) {
-          		assert.fail('Failed to get the response');
-          	} else {
-              assert.equal(resp.statusCode, 200);
-              var pbody = JSON.parse(body);
-              assert((pbody.city === 'Hamilton'), "City name does not match");
-              done();
-            }
+		it("check report title", function(done) {
+      driver.get(DEMODRAURL).then(function() {
+        driver.getTitle().then(function(title) {
+          assert.equal(title, 'DemoDRA - Current Weather');
+          done();
         });
-    	});
-
-      it('without zip code', function(done) {
-        if(!appUrl) {
-            assert.fail("Environment variable APP_URL is not defined");
-            return done();
-        }
-        request({
-      		method: 'GET',
-              url: appUrl + '/api/v1/getWeather'
-          }, /* @callback */ function(err, resp, body) {
-          	if(err) {
-          		assert.fail('Failed to get the response');
-          	} else {
-              assert.equal(resp.statusCode, 400);
-              done();
-            }
-        });
-    	});
-
-      it('with another valid zip code', function(done) {
-        if(!appUrl) {
-            assert.fail("Environment variable APP_URL is not defined");
-            return done();
-        }
-        request({
-      		method: 'GET',
-              url: appUrl + '/api/v1/getWeather?zip=Auckland'
-          }, function(err, resp, body) {
-          	if(err) {
-          		assert.fail('Failed to get the response');
-          	} else {
-              assert.equal(resp.statusCode, 200);
-              var pbody = JSON.parse(body);
-              assert(pbody.city === 'Auckland', "City name does not match");
-              done();
-            }
-        });
-    	});
+      });
     });
-})();
+
+    it("Check for zip4 element", function(done) {
+      driver.isElementPresent(webdriver.By.id('zip4')).then(function(present) {
+        assert.equal(present, true);
+        done();
+      });
+    });
+
+    it("Check for Weather text", function(done) {
+      driver.findElement(webdriver.By.id('weather')).getText().then(function(text) {
+        assert.equal(text, "Weather");
+        done();
+      });
+    });
+
+    it("Enter zip code and check output - 1", function(done) {
+      driver.findElement(webdriver.By.id('zip1')).sendKeys('78613');
+      driver.findElement(webdriver.By.id('zip1city')).getText().then(function(text) {
+        assert.equal(text, ' Anderson Mill');
+        done();
+      });
+    });
+
+    it("Enter zip code and check output - 2", function(done) {
+      driver.findElement(webdriver.By.id('zip4')).sendKeys('75038');
+      driver.findElement(webdriver.By.id('zip4city')).getText().then(function(text) {
+        assert.equal(text, ' Irving');
+        done();
+      });
+    });
+
+    after(function(done) {
+      driver.getSession().then(function(session) {
+        var jobId = session.getId();
+        //console.log(jobId);
+        driver.quit();
+        saucelab.stopJob(jobId, {}, function(){});
+        done();
+      });
+    });
+
+	});;
+}());
