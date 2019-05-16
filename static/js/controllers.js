77321f11fce2d40898c3c0205162f6097309a33d
shizhen wang <353311544@qq.com>
2019-05-16 02:04:57 +0000

    kewu

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -29,11 +29,11 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
     	var myLatLng = event.latLng;
     	var lat = myLatLng.lat();
     	var lon = myLatLng.lng();
-    	alert(lat);
-    	alert(typeof lon);
-    	   //if(lat<-35.22676 && lat>-46.56069 && lon>-176.55973 && lon<178.00417){
+    	//alert(lat);
+    	//alert(typeof lon);
+    	   if(lat<-35.22676 && lat>-46.56069 && lon>-176.55973 && lon<178.00417){
     	placeMarker(event.latLng);
-		//}
+		}
 
    
     });
